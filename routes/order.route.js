9184b895bf35e35e4f4e5cf3422191dc4d1fb22f
ayaelhenawy <elhenawyaya21@gmail.com>
2025-10-02 03:18:23 +0300

    update order to paied, delivered

--- routes/order.route.js
+++ routes/order.route.js
@@ -1,6 +1,6 @@
 import express from "express";
 import { allowTo, protectedRoutes } from "../controllers/auth.controller.js";
-import { createCashOrder, getAllOrders, getLoggedUserOrders, getSpecificOrder } from "../controllers/order.controller.js";
+import { createCashOrder, getAllOrders, getLoggedUserOrders, getSpecificOrder, updateOrderToDelivered, updateOrderToPaid } from "../controllers/order.controller.js";
 
 const router = express.Router();
 
@@ -10,7 +10,8 @@ router.post("/cash/:cartId", createCashOrder);
 router.get("/",getAllOrders); 
 router.get("/:orderId",getSpecificOrder);
 router.get("/my/orders",getLoggedUserOrders);
-
+router.put("/:id/pay",updateOrderToPaid);
+router.put("/:id/deliver",updateOrderToDelivered);
 
 
 export default router;
